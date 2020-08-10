e2564502f5e7dde5794998e3e3148016b077c209
Duksun Choi <duksun.choi@telus.com>
2020-08-10 12:14:09 -0400

    test5

--- spring-boot-demo1/src/main/java/com/example/demo3/BookController.java
+++ spring-boot-demo1/src/main/java/com/example/demo3/BookController.java
@@ -3,6 +3,7 @@ package com.example.demo3;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,50 +20,48 @@ import com.example.persistence.repo.BookRepository;
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
- 
+
     @Autowired
     private BookRepository bookRepository;
- 
+
     @GetMapping
-    public Iterable findAll() {
+    public Iterable<Book> findAll() {
         return bookRepository.findAll();
     }
- 
+
     @GetMapping("/title/{bookTitle}")
-    public List findByTitle(@PathVariable String bookTitle) {
+    public List<Book> findByTitle(@PathVariable String bookTitle) {
         return bookRepository.findByTitle(bookTitle);
     }
- 
-    /*
+
     @GetMapping("/{id}")
-    public Book findOne(@PathVariable Long id) {
+    public Book findOne(@PathVariable long id) {
         return bookRepository.findById(id)
           .orElseThrow(BookNotFoundException::new);
     }
- 
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Book create(@RequestBody Book book) {
-        return bookRepository.save(book);
+        Book book1 = bookRepository.save(book);
+        return book1;
     }
- 
+
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
+    public void delete(@PathVariable long id) {
         bookRepository.findById(id)
           .orElseThrow(BookNotFoundException::new);
         bookRepository.deleteById(id);
     }
- 
+
     @PutMapping("/{id}")
-    public Book updateBook(@RequestBody Book book, @PathVariable Long id) {
+    public Book updateBook(@RequestBody Book book, @PathVariable long id) {
         if (book.getId() != id) {
-          throw new BookIdMismatchException();
+            throw new BookIdMismatchException();
         }
         bookRepository.findById(id)
           .orElseThrow(BookNotFoundException::new);
         return bookRepository.save(book);
     }
-    
-    */
 }
 
