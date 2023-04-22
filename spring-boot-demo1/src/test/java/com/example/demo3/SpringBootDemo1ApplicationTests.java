db26f1d0c9df945e2ae33e884b6fd37eba1b491d
travi <travi@DESKTOP-53FIITR>
2023-04-22 08:50:12 -0400

    adding junit testing

--- spring-boot-demo1/src/test/java/com/example/demo3/SpringBootDemo1ApplicationTests.java
+++ spring-boot-demo1/src/test/java/com/example/demo3/SpringBootDemo1ApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.demo3;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringBootDemo1ApplicationTests {
+public class SpringBootDemo1ApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
---
+++ spring-boot-demo1/src/test/java/com/example/demo3/TestWebApp.java
@@ -0,0 +1,38 @@
+package com.example.demo3;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+public class TestWebApp extends SpringBootDemo1ApplicationTests {
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+	
+	@Test
+	public void testBooks() throws Exception {
+		mockMvc.perform(get("/api/books")).andExpect(status().isOk());
+		
+//				.andExpect(content().contentType("application/json;charset=UTF-8"))
+//				.andExpect(jsonPath("$.title").value("title1")).andExpect(jsonPath("$.author").value("author1"));
+					
+//				.andExpect(jsonPath("$.empId").value("1")).andExpect(jsonPath("$.salary").value(3000));
+
+	}
+
+}
